78634f6b6e30ae1da3b12106383603c0857200a6
Alexander Morales <a@mojitoo.com>
2019-10-14 14:29:46 -0700

    Refactor Blogcontext with automatic context creation.

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,6 +1,5 @@
-import React, { useReducer } from "react";
-
-const BlogContext = React.createContext();
+import React from "react";
+import createDataContext from "./createDataContext";
 
 const blogReducer = (state, action) => {
   switch (action.type) {
@@ -11,16 +10,10 @@ const blogReducer = (state, action) => {
   }
 };
 
-export const BlogProvider = ({children}) => {
-  const [blogPosts, dispatch] = useReducer(blogReducer, []);
-
-  const addBlogPost = () => {
+const addBlogPost = (dispatch) => {
+  return () => {
     dispatch({ type: 'add_blogpost' });
   };
+};
 
-  return <BlogContext.Provider value={{ data: blogPosts, addBlogPost }}>
-    {children}
-  </BlogContext.Provider>
-}
-
-export default BlogContext;
+export const { Context, Provider } = createDataContext(blogReducer, { addBlogPost }, []);
