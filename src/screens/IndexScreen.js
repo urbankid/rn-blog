27a4345e7d86423e0daf8eb601a9f858ff768eab
Alexander Morales <a@mojitoo.com>
2019-10-14 17:30:17 -0700

    Add show screen to BlogContext.

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -3,7 +3,7 @@ import { View, Text, StyleSheet, FlatList, Button, TouchableOpacity } from "reac
 import {Context } from "../context/BlogContext";
 import { Feather } from "@expo/vector-icons";
 
-const IndexScreen = () => {
+const IndexScreen = ({navigation}) => {
   const { state, addBlogPost, deleteBlogPost } = useContext(Context);
 
   return (
@@ -13,13 +13,16 @@ const IndexScreen = () => {
         data={state}
         keyExtractor={blogPost => blogPost.id.toString()}
         renderItem={({item}) => {
-          return <View style={styles.row}>
-            <Text style={styles.title}>{item.title} - {item.id}</Text>
-            <TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
-              <Feather name="trash" style={styles.icon} />
-            </TouchableOpacity>
-          </View>
-        }}
+          return (
+          <TouchableOpacity onPress={() => navigation.navigate('Show', { id: item.id })}>
+            <View style={styles.row}>
+              <Text style={styles.title}>{item.title} - {item.id}</Text>
+              <TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
+                <Feather name="trash" style={styles.icon} />
+              </TouchableOpacity>
+            </View>
+          </TouchableOpacity>
+          )}}
       />
     </View>
   );
